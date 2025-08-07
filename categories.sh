97a918a755c1d3380a31417f3109cbd80d5a614d
Zayar Lin <zayar.ln@outlook.com>
2025-08-07 22:07:08 +0630

    Create categories.sh

---
+++ categories.sh
@@ -0,0 +1,27 @@
+# ~/.bashrc.d/categories.sh
+
+declare -A CMD_CATEGORIES=(
+  [ls]="File Ops"
+  [cp]="File Ops"
+  [mv]="File Ops"
+  [rm]="File Ops"
+  [mkdir]="File Ops"
+  [mount]="Filesystem"
+  [ps]="Process"
+  [kill]="Process"
+  [ping]="Network"
+  [ip]="Network"
+  [uname]="System Info"
+  [uptime]="System Info"
+  [shutdown]="Boot"
+  [tar]="Archive"
+  [gzip]="Archive"
+  [grep]="Text"
+  [awk]="Text"
+  [bash]="Shell"
+  [sudo]="Security"
+  [chmod]="Security"
+  [udevadm]="Device"
+  [date]="Misc"
+)
+
