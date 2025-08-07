e1d4f48e6cbb2a208602eaeee6a0da52d00f8835
Zayar Lin <zayar.ln@outlook.com>
2025-08-07 22:03:41 +0630

    Create colors.sh

---
+++ colors.sh
@@ -0,0 +1,12 @@
+# ~/.bashrc.d/colors.sh
+
+C_RESET='\[\e[0m\]'
+C_RED='\[\e[0;31m\]'
+C_GREEN='\[\e[0;32m\]'
+C_YELLOW='\[\e[1;33m\]'
+C_BLUE='\[\e[0;34m\]'
+C_PURPLE='\[\e[0;35m\]'
+C_CYAN='\[\e[0;36m\]'
+C_BOLD='\[\e[1m\]'
+
+export C_RESET C_RED C_GREEN C_YELLOW C_BLUE C_PURPLE C_CYAN C_BOLD
